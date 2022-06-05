7bd69cf2e0de2deda2e6e366a48073d25f3b0ad6
adarm12 <adarm1234@gmail.com>
2022-06-05 15:50:05 +0300

    1

--- src/main/java/StatusMessage.java
+++ src/main/java/StatusMessage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import javax.swing.*;
 import javax.swing.plaf.ComponentUI;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.WeakHashMap;
 
@@ -31,22 +32,18 @@ public class StatusMessage extends JPanel {
                 while (!this.isSeen) {
                     WebElement messageBox = web.findElement(By.id("main"));
                     List<WebElement> tagMessage = messageBox.findElements(By.className("_1beEj"));
-
                     List<WebElement> afterTag = tagMessage.get(tagMessage.size() - 1).findElements(By.tagName("span"));
                     String statusWeb = afterTag.get(1).getAttribute("aria-label");
-
                     new Thread(() -> {
                         if (statusWeb.contains("נמסרה")) {
                             this.isAccepted = true;
                             this.status.setText("נמסרה");
-                            System.out.println("נמסרה");
                         } else if (statusWeb.contains("נקראה")) {
                             this.isSeen = true;
                             this.status.setText("נקראה");
-                            System.out.println("נקראה");
+                            incomingMessage(web);
                         }
                     }).start();
-
                 }
             }).start();
             Thread.sleep(3000);
@@ -54,6 +51,26 @@ public class StatusMessage extends JPanel {
             status(web);
         }
     }
+
+    public void incomingMessage(ChromeDriver web) {
+        try {
+            new Thread(() -> {
+                List<WebElement> messagesList = web.findElements(By.className("_3K4-L"));
+                System.out.println("list" + messagesList.size());
+                List<WebElement> incomingMessages = messagesList.get(0).findElements(By.className("_22Msk"));
+                System.out.println("in" + incomingMessages.size());
+                List<WebElement> lastTag = incomingMessages.get(0).findElements(By.className("_1Gy50"));
+                System.out.println("last" + lastTag.size());
+                List<WebElement> tag = lastTag.get(0).findElements(By.cssSelector("#main > div._2gzeB > div > div._33LGR > div._3K4-L > div:nth-child(17) > div > div.Nm1g1._22AX6 > div._22Msk > div.copyable-text > div > span.i0jNr.selectable-text.copyable-text"));
+                System.out.println("text: " + tag.get(0).getText());
+//                String tagText = tag.get(tag.size() - 1).getAttribute("dir");
+//                System.out.println(tagText);
+            }).start();
+        } catch (Exception e) {
+            incomingMessage(web);
+        }
+    }
+
 
     public JLabel getStatus() {
         return status;
